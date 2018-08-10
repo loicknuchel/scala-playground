e4bc0a4d28cc0c13b0877542cc3f165652cb7a70
Loïc Knuchel <l.knuchel@criteo.com>
2018-08-10 14:25:30 +0200

    [FP to the max] make program safe with correct types

--- src/main/scala/playground/fpToTheMax/Main.scala
+++ src/main/scala/playground/fpToTheMax/Main.scala
@@ -1,11 +1,15 @@
 package playground.fpToTheMax
 
 import scala.io.StdIn.readLine
+import scala.util.Try
 
 /**
   * Step0: Imperative code that do side effects and throw errors
+  * Step1: Make the program safe and not throw errors
   */
 object Main {
+  def parseInt(in: String): Option[Int] = Try(in.toInt).toOption
+
   def main(args: Array[String]): Unit = {
     println("What is your name?")
 
@@ -20,16 +24,25 @@ object Main {
 
       println("Dear " + name + ", please guess a number from 1 to 5:")
 
-      val guess = readLine().toInt
+      parseInt(readLine()) match {
+        case None => println("You did not enter a number")
+        case Some(guess) =>
+          if (guess == num) println("You guessed right, " + name + "!")
+          else println("You guessed wrong, " + name + "! The number was: " + num)
+      }
 
-      if (guess == num) println("You guessed right, " + name + "!")
-      else println("You guessed wrong, " + name + "! The number was: " + num)
+      var cont = true
 
-      println("Do you want to continue, " + name + "?")
+      while (cont) {
+        cont = false
 
-      readLine() match {
-        case "y" => exec = true
-        case "n" => exec = false
+        println("Do you want to continue, " + name + "?")
+
+        readLine().toLowerCase match {
+          case "y" => exec = true
+          case "n" => exec = false
+          case _ => cont = true
+        }
       }
     }
   }
